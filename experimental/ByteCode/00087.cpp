d882fda8e173fceb3b749e412d345071281fcfdd
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-05-04 10:34:15 +0900

    experimental: add a test case for bytecode interpreter

---
+++ experimental/ByteCode/00087.cpp
@@ -0,0 +1,18 @@
+// RUN: clang++ -c -fexperimental-new-constant-interpreter %s
+// EXPECT-CRASH-ASSERT: Pointer
+// EXPECT-CRASH-ASSERT: Pointee
+
+namespace std {
+template <class _E> class initializer_list {};
+} // namespace std
+
+template <int> struct S {};
+template <> struct std::initializer_list<S<2>> {
+  const S<2> *first;
+  const S<2> *last;
+};
+
+constexpr auto foo() {
+  constexpr auto bar = {S<2>{}, S<2>{}, S<2>{}};
+  static_assert(bar.first + 3 == bar.last, "");
+}
