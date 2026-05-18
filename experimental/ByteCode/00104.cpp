a57f1529ead429683913b34e437ba3240486ff5f
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-05-18 19:07:53 +0900

    experimental: add 2 test cases for bytecode interpreter

---
+++ experimental/ByteCode/00104.cpp
@@ -0,0 +1,20 @@
+// RUN: clang++ -c -fexperimental-new-constant-interpreter %s
+// EXPECT-CRASH-ASSERT: initialize
+// EXPECT-CRASH-ASSERT: BS.Base
+// EXPECT-CRASH-ASSERT: composite
+
+using u8 = unsigned char;
+
+template <typename T> struct R {
+  T CO, V;
+  constexpr bool operator==(const R<T> &O) { return CO == O.CO && V == O.V; }
+};
+
+constexpr R<u8> subcb(u8 lhs, u8 rhs, u8 carry) {
+  u8 carry_out{}, sum{};
+  // sum = __builtin_subcb(lhs, rhs, carry, &carry_out);
+  sum = __builtin_subcb(lhs, rhs, carry, &rhs);
+  return {carry_out, sum};
+}
+
+static_assert(subcb(10, 15, 1) == R<u8>{1, 250});
