eb1ba43e5e48037a6fb15890d38d5747dccaa3fc
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-05-31 15:07:40 +0900

    experimental: add a test case for bytecode interpreter

---
+++ experimental/ByteCode/00115.cpp
@@ -0,0 +1,25 @@
+// RUN: clang++ -c -fexperimental-new-constant-interpreter %s
+// EXPECT-CRASH-ASSERT: CheckActive
+// EXPECT-CRASH-ASSERT: Ptr.inUnion
+
+struct B {};
+struct M1 {
+  int foo[3];
+};
+struct M3 : M2 {
+  char bar[3];
+};
+struct M4 {
+  int baz[3];
+};
+struct M5 : M4, M3 {
+  char qux[3];
+};
+struct S : M1, M5 {
+  bool d;
+};
+
+constexpr S s = {};
+constexpr M5 *m5 = (M5 *)((B *)&s);
+
+static_assert(m5->qux[0] == 0);
