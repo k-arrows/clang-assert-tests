07b15d9d1a6bf11d382d70b38b344cd5e14f1a95
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-06-14 11:28:21 +0900

    cases: add a test case

---
+++ cases/00356.cpp
@@ -0,0 +1,17 @@
+// RUN: clang++ -c -std=c++20 %s
+// EXPECT-CRASH-ASSERT: hasAcceptableMemberSpecialization
+// EXPECT-CRASH-ASSERT: getDeclContext
+// EXPECT-CRASH-ASSERT: not
+
+template <typename T> struct A {
+  void B { int y = 0; };
+  template <typename U> int x = 0;
+}
+
+template <typename U>
+struct A<long>::B<U &> {
+};
+template <typename U> int A<int>::x = 3;
+template <> template <typename U> int A<int>::x<U &> = 4;
+
+static_assert(A<long>::x<int &> == 5);
