b89b1a579186d0add63f0f2d1c78a36d3a05dcd3
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-06-27 20:21:51 +0900

    experimental: add a test case for bytecode interpreter

---
+++ experimental/ByteCode/00154.cpp
@@ -0,0 +1,13 @@
+// RUN: clang++ -c -std=c++20 -fexperimental-new-constant-interpreter %s
+// EXPECT-CRASH-NOASSERT
+
+struct A {
+  constexpr A(int) {}
+};
+
+struct B : A {
+  A a;
+  int i;
+};
+
+constexpr B b(42, 43);
