6c9e6dc966f241fda737d6674084d7c68f4cb3f0
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-06-28 23:23:11 +0900

    cases: add a test case

---
+++ cases/00382.cpp
@@ -0,0 +1,13 @@
+// RUN: clang++ -c -std=c++20 %s
+// EXPECT-CRASH-NOASSERT
+
+class Dim;
+struct S;
+template <class T> concept foo = true;
+template <class T> concept SS = false;
+template <class X> concept _Dim = _Dim<X>;
+template <SS, _Dim Dim, foo<> E> auto bar(Dim, E) {};
+
+void baz() {
+  auto qux = bar<S>(true, [] {});
+}
