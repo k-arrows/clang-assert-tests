c559b5d660f85388b4ef905d3fa905ed354175a4
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-07-11 20:05:52 +0900

    cases: add a test case

---
+++ cases/00424.cpp
@@ -0,0 +1,14 @@
+// RUN: clang++ -c %s
+// EXPECT-CRASH-ASSERT: CheckClassTemplate
+// EXPECT-CRASH-ASSERT: PrevClassTemplate
+// EXPECT-CRASH-ASSERT: specialization
+
+class C {
+  class P {};
+
+  template <typename T1> class FT {
+    template <typename T2> class BT1 {};
+  };
+
+  template <> template <typename..., foo> class FT<C::P>::BT2 {};
+};
