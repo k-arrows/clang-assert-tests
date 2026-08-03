4db2f9c5688cd35f2577fda2c27d0406ce42dbeb
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-08-03 12:58:34 +0900

    cases: skip some test cases on aarch64

--- cases/00469.cpp
+++ cases/00469.cpp
@@ -2,6 +2,7 @@
 // EXPECT-CRASH-ASSERT: execute
 // EXPECT-CRASH-ASSERT: Op2.second
 // EXPECT-CRASH-ASSERT: Division
+// SKIP: aarch64
 
 void foo() {
   __asm { return 1 / 0; }
--- cases/00484.cpp
+++ cases/00484.cpp
@@ -2,6 +2,7 @@
 // EXPECT-CRASH-ASSERT: parseMSInlineAsm
 // EXPECT-CRASH-ASSERT: AsmStart
 // EXPECT-CRASH-ASSERT: Expected
+// SKIP: aarch64
 
 void foo() {
   __asm { return 1f; }
