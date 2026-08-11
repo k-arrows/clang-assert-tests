d098293434758e04cb3259bd7e5682867ce9cf3d
k-arrows <121552426+k-arrows@users.noreply.github.com>
2026-08-11 14:28:32 +0900

    experimental: update 2 test cases to match latest LLVM behavior

--- experimental/ByteCode/00159.cpp
+++ experimental/ByteCode/00159.cpp
@@ -1,6 +1,5 @@
 // RUN: clang++ -c -fexperimental-new-constant-interpreter %s
-// EXPECT-CRASH-ASSERT: peek
-// EXPECT-CRASH-ASSERT: toPrimType
+// EXPECT-FAIL
 
 auto foo = [](bar) { int j; return j; };
 constexpr int (*baz)(int) = foo;
--- experimental/ByteCode/00165.cpp
+++ experimental/ByteCode/00165.cpp
@@ -1,6 +1,5 @@
 // RUN: clang++ -c -fexperimental-new-constant-interpreter %s
-// EXPECT-CRASH-ASSERT: getSize
-// EXPECT-CRASH-ASSERT: isUnknownSizeArray
+// EXPECT-FAIL
 
 constexpr const char foo[] = {bar};
 
